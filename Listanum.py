9ce7dd9b61b8577fa5cebd424f5bd0e67fea1f2d
VinnieSousa <ovinnie1@hotmail.com>
2024-03-07 20:51:58 -0300

    added listanome and Listanum

---
+++ Listanum.py
@@ -0,0 +1,5 @@
+lista = []
+for i in range(5):
+    numero = int(input("Digite um numero inteiro: "))
+    lista.append(numero)
+print("Lista de números inseridos; ", lista)
---
+++ listanome.py
@@ -0,0 +1,3 @@
+nomes = input("Digite os nomes separados por vírgulas: ")
+lista = nomes.split(",")
+print("Lista de nomes: ", lista)
